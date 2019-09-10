20af216b42225c65eec9135904be4e2ebc92d2fd
Mueed Ur Rehman <mueedramay@users.noreply.github.com>
2019-09-10 02:24:44 -0400

    Added more solutions

---
+++ CS 5199.py
@@ -0,0 +1,9 @@
+#DFS based solution
+#Search based enumeration
+# def mul2add2(n,m):
+#     if m < n :
+#         return False
+#     elif 
+
+# def abc1234(n):
+print(3+2)
--- HackerRankSolutions.py
+++ HackerRankSolutions.py
@@ -88,6 +88,7 @@ l1  = [1,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7]
 # print(droppedRequests(l2))
 # print(droppedRequests([1,1,1,1,2,2,2,3,3,3,4,4,4,11,11,11,6,6,6,5,5,5]))
 
+#Broadway Technology HackerRank
 def smallestString(weight):
     multiplier = 3
     weights = [1] * 26
@@ -139,3 +140,121 @@ def smallestString2(weight):
     return string[::-1] #reverse the string to get the lexicographically smallest and return it
 
 # print(smallestString2(25))
+#Akuna Data Infrastructure Developer HackerRank
+class node:
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+D = node("D")
+G = node("G")
+H = node("H")
+E = node("E")
+E.left = G
+E.right = H
+B = node("B")
+B.left = D
+B.right = E
+I = node("I")
+F = node("F")
+F.left = I
+C = node("C")
+C.right = F
+A = node("A")
+A.left = B
+A.right = C
+
+def f(node):
+    if node is None:
+        return
+    f(node.left)
+    f(node.right)
+    print(node.val)
+
+# f(A)
+
+def check_power_two(val):
+    print("1")
+    print(val | (val + 1) == val)
+    print("2")
+    print(val & (val + 1) == val)
+    print("3")
+    print(val & (val - 1) == 0)
+    print("4")
+    print(val | (val - 1) == 0)
+    # print("5")
+    # print(val >> 1 == val/2)
+    # print("6")
+    # print(((val >> 1) << 1) == val)
+
+check_power_two(31)
+
+# string = "0000016c052dcf41"
+# print(int(string,16))
+# print(len("0000016c052dcf410"))
+
+#Got a list in hex, main function converted to a byte array. You had to parse the array and rebuild the DML Events
+def convert_wal(s):
+    results = []
+    start = 0
+    not_end = True
+    #Note, I had to convert to hex to get it to work as the input really was a byte array.
+    while (not_end):
+        result = ""
+        epoch_end = start + 16
+        epoch = s[start: epoch_end]
+        epoch = str(int(epoch, 16))
+        result = result + epoch
+        mid_start = epoch_end
+        mid_end = epoch_end + 2
+        mid = s[mid_start:mid_end]
+        message = "INSERT"
+        if mid == "01":
+            message = "UPSERT"
+        elif mid == "02":
+            message = "DELETE"
+        result = result + "|" + message
+        key_length_start = mid_end
+        key_length_end = mid_end + 4
+        key_length = s[key_length_start:key_length_end]
+        key_length = int(key_length,16)
+        key_string_start = key_length_end
+        key_string_end = key_string_start + (key_length * 2)
+        key_string_ascii = s[key_string_start: key_string_end]
+        key = ""
+        for i in range(0,len(key_string_ascii),2):
+            encoded = key_string_ascii[i:i+2]
+            code = int(encoded, 16)
+            char = chr(code)
+            key = key + char
+        result = result + "|" + key
+        if message != "DELETE":
+            value_length_start = key_string_end
+            value_length_end = value_length_start + 4
+            value_length = s[value_length_start:value_length_end]
+            value_length = int(value_length, 16)
+            value_string_start = value_length_end
+            value_string_end = value_string_start + (value_length * 2)
+            value_string_ascii = s[value_string_start: value_string_end]
+            value = ""
+            for i in range(0, len(value_string_ascii), 2):
+                encoded = value_string_ascii[i:i + 2]
+                code = int(encoded, 16)
+                char = chr(code)
+                value = value + char
+            result = result + "|" + value
+            start = value_string_end
+        else:
+            start = key_string_end
+        results.append(result)
+        if start == len(s):
+            not_end = False
+    # print(results)
+    a = 1
+
+# print(len("0000016c052dcf4102000f746573745f6b65795f313233383937"))
+# convert_wal("0000016c052dcf4100000e746573745f6b65795f30393831320010746573745f76616c75655f31323837360000016c052dcf4102000f746573745f6b65795f313233383937")
+# print(bytes.fromhex('deadbeef'))
+
+# print(sorted(set([1,2,9,1,2,3,1,4,1,5,7])))
